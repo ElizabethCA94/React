fd1012e6d980442ee3864ab53cc03504f6f19536
ElizabethCA94 <ecarreno@unal.edu.co>
2021-01-14 19:00:18 -0500

    Add component react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function getButtonText(){
+  return 'clickMe!'
+}
+
+//create react component
+const buttonText = 'TextMe!';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <label class="label" for="name">
+          Enter name:
+        </label>
+        <input id="name"></input>
+  <button style = {{ backgroundColor:'blue', color:'white' }}>{getButtonText()}</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  //<React.StrictMode>
+    <App />,
+  //</React.StrictMode>,
+  document.querySelector('#root')
 );
 
+
+//Take de react component and show it on the screen
+/*ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+
+);*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
