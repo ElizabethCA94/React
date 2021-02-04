84c3ec3a51c373e64f4e29ac22e564e1d6892792
ElizabethCA94 <ecarreno@unal.edu.co>
2021-02-04 18:50:51 -0500

    add styles and update package.json

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Button, Icon, Image } from 'semantic-ui-react';
+import logo from '../../assets/images/logo.svg';
 
 const Header = () => {
     return (
-        <Menu>
-            <Menu.Item style={{fontSize : 24}}>Learn Functions</Menu.Item>
-            <Menu.Item>Create quiz</Menu.Item>
+        <Menu secundary pointing>
+            <Image src = "logo" width = {60}></Image>
+            <Menu.Item style = {{fontSize : 24}}>Learn Functions</Menu.Item>
+            <Menu.Item position = "right">
+                <Button icon>
+                    <Icon name = "add"></Icon>
+                    Create quiz
+                </Button></Menu.Item>
             <Menu.Item>Logout</Menu.Item>
 
         </Menu>
