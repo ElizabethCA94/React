39ee139e42ab09f9781ab788feadb9545723e946
ElizabethCA94 <ecarreno@unal.edu.co>
2021-02-13 13:18:01 -0500

    fix update quiz flow

--- src/layout/EditQuiz/index.js
+++ src/layout/EditQuiz/index.js
@@ -1,75 +1,86 @@
-import React, { useState, useContext } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import React, { useState, useContext } from "react";
+import { Form, Button } from "semantic-ui-react";
 import Context from "../../store/context";
 //import './index.css';
 
-
 const EditQuizUI = () => {
   const { state } = useContext(Context); // global variable
-  
+
   const [description, setdescription] = useState(state.activeQuizDescription);
-  const [functionParams, setfuctionParams] = useState(state.activeQuizFunctionParams);
-  const [expectedOutput, setexpectedOutput] = useState(state.activeQuizExpectedOutput);
+  const [functionParams, setfuctionParams] = useState(
+    state.activeQuizFunctionParams
+  );
+  const [expectedOutput, setexpectedOutput] = useState(
+    state.activeQuizExpectedOutput
+  );
 
   const handleDescriptionChange = (e) => {
-    setdescription(e.target.value)
-  }
+    setdescription(e.target.value);
+  };
 
   const handleFuctionParamsChange = (e) => {
-    setfuctionParams(e.target.value)
-  }
+    setfuctionParams(e.target.value);
+  };
 
   const handleExpectedOutputChange = (e) => {
-    setexpectedOutput(e.target.value)
-  }
-  
+    setexpectedOutput(e.target.value);
+  };
+
   const onSubmit = async () => {
-    await fetch('http://localhost:4000/dev/createQuiz', {
-      method: 'post',
+    const updateResponse = await fetch("http://localhost:4000/dev/updateQuiz", {
+      method: "post",
       body: JSON.stringify({
         description,
         functionParams,
         expectedOutput,
-        userSecretId: state.secretId
+        userSecretId: state.secretId,
+        quizId: state.activeQuizId,
       }),
       headers: {
-        'Content-Type': 'application/json',
-      }
-    }).then(response => response.json())
-      .then(data => data)
-      .catch(e => console.log(e))
-
-  }
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((e) => console.log(e));
+    
+    console.log(updateResponse);
+  };
 
   return (
     <div class="ui form">
       <h1>Editar Quiz</h1>
       <div class="lf-register-form">
-        <Form onSubmit = {onSubmit}>
+        <Form onSubmit={onSubmit}>
           <Form.Field>
             <div class="ui form success">
               <div class="field">
                 <label>Enunciado</label>
-                <textarea value = {description} 
-                onChange = {handleDescriptionChange} 
-                type="form-textarea-control-opinion"
-                placeholder="Escribe una función que retorne un 6 (a + b)"/>
+                <textarea
+                  value={description}
+                  onChange={handleDescriptionChange}
+                  type="form-textarea-control-opinion"
+                  placeholder="Escribe una función que retorne un 6 (a + b)"
+                />
               </div>
             </div>
           </Form.Field>
           <Form.Field>
             <label>Parámetros de la función</label>
-            <input value = {functionParams} 
-            onChange = {handleFuctionParamsChange} 
-            type="form-input-control-opinion" 
-            placeholder= "a = 5, b = 1"/>
+            <input
+              value={functionParams}
+              onChange={handleFuctionParamsChange}
+              type="form-input-control-opinion"
+              placeholder="a = 5, b = 1"
+            />
           </Form.Field>
           <Form.Field>
             <label>Respuesta esperada</label>
-            <input value = {expectedOutput} 
-            onChange = {handleExpectedOutputChange} 
-            type="form-input-control-opinion" 
-            placeholder= "6"
+            <input
+              value={expectedOutput}
+              onChange={handleExpectedOutputChange}
+              type="form-input-control-opinion"
+              placeholder="6"
             />
           </Form.Field>
           <Button type="ui submit button">Submit</Button>
